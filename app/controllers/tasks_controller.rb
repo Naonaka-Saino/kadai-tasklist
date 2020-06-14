7d38ad3232608868cde75ba1ba1a42a6b0d1ae98
nao.saino <saino305@gmail.com>
2020-06-14 17:23:49 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,16 @@
 class TasksController < ApplicationController
   
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-        @tasks = Task.all
+    if logged_in?
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
-      @task = Task.find(params[:id])
+   @task=current_user.tasks.find(params[:id])
   end
 
   def new
@@ -17,12 +20,13 @@ class TasksController < ApplicationController
 #newのページから送信されるフォームを処理する。
   def create
       #これにフォーム中身が入っている。
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = '保存したぞ、絶対やれ'
-      redirect_to @task
+      redirect_to root_url
     else
+      
       flash.now[:danger] = '保存されなかったぞ'
       render :new
     end
@@ -37,22 +41,30 @@ class TasksController < ApplicationController
       if @task.update(task_params)
       flash[:success] = '編集したぞ、意思の弱いやつめ'
       redirect_to @task
-    else
+      else
+        #これ何？
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = '編集されなかったぞ'
       render :edit
-    end
-
+      end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     flash[:success] = '消したぞ、意思の弱いやつめ'
-    redirect_to tasks_url
+    redirect_to tasks_path
   end
   #ストロングパラメータ
+  private
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
